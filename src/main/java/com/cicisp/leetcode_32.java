7ebf1c2e60783241341848043cba47f47327be49
wangjj <lzx4627@163.com>
2020-07-07 14:39:17 +0800

    2020/07/07 secondCommit

---
+++ src/main/java/com/cicisp/leetcode_32.java
@@ -0,0 +1,83 @@
+package com.cicisp;
+
+/**
+ * 给定一个只包含 '(' 和 ')' 的字符串，找出最长的包含有效括号的子串的长度。
+
+示例 1:
+
+输入: "(()"
+输出: 2
+解释: 最长有效括号子串为 "()"
+示例 2:
+
+输入: ")()())"
+输出: 4
+解释: 最长有效括号子串为 "()()"
+*/
+public class leetcode_32 {
+    //  动态规划
+    // public int longestValidParentheses(String s) {
+    //     int maxans = 0;
+    //     int dp[] = new int[s.length()];
+    //     for (int i = 1; i < s.length(); i++) {
+    //         if (s.charAt(i) == ')') {
+    //             if (s.charAt(i - 1) == '(') {
+    //                 dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+    //             } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+    //                 dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+    //             }
+    //             maxans = Math.max(maxans, dp[i]);
+    //         }
+    //     }
+    //     return maxans;
+    // }
+    // 栈
+    // public int longestValidParentheses(String s) {
+    //     int maxans = 0;
+    //     Stack<Integer> stack = new Stack<>();
+    //     stack.push(-1);
+    //     for (int i = 0; i < s.length(); i++) {
+    //         if (s.charAt(i) == '(') {
+    //             stack.push(i);
+    //         } else {
+    //             stack.pop();
+    //             if (stack.empty()) {
+    //                 stack.push(i);
+    //             } else {
+    //                 maxans = Math.max(maxans, i - stack.peek());
+    //             }
+    //         }
+    //     }
+    //     return maxans;
+    // }
+    // 正向逆向结合
+    public int longestValidParentheses(String s) {
+        int left = 0, right = 0, maxlength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = Math.max(maxlength, 2 * right);
+            } else if (right > left) {
+                left = right = 0;
+            }
+        }
+        left = right = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = Math.max(maxlength, 2 * left);
+            } else if (left > right) {
+                left = right = 0;
+            }
+        }
+        return maxlength;
+    }
+}
