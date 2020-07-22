16c0813d194050f31eb6a4c76cf9962cef011388
wangjj <lzx4627@163.com>
2020-07-22 09:10:07 +0800

    2020/07/22 first_commit

---
+++ src/main/java/com/cicisp/exercise/leetcode_154.java
@@ -0,0 +1,37 @@
+package com.cicisp.exercise;
+
+/**
+ * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+
+( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+
+请找出其中最小的元素。
+
+注意数组中可能存在重复的元素。
+
+示例 1：
+
+输入: [1,3,5]
+输出: 1
+示例 2：
+
+输入: [2,2,2,0,1]
+输出: 0
+ */
+public class leetcode_154 {
+    public int minArray(int[] numbers) {
+        int low = 0;
+        int high = numbers.length - 1;
+        while (low < high) {
+            int pivot = low + (high - low) / 2;
+            if (numbers[pivot] < numbers[high]) {
+                high = pivot;
+            } else if (numbers[pivot] > numbers[high]) {
+                low = pivot + 1;
+            } else {
+                high -= 1;
+            }
+        }
+        return numbers[low];
+    }
+}
