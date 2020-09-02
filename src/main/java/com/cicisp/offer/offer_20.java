cde80d0d3961dcd167fdf44863263e87a2e96b6c
wangjj <lzx4627@163.com>
2020-09-02 09:35:50 +0800

    :sparkles: feat:offer-20.表示数值的字符串

---
+++ src/main/java/com/cicisp/offer/offer_20.java
@@ -0,0 +1,38 @@
+package com.cicisp.offer;
+
+/**
+ * 请实现一个函数用来判断字符串是否表示数值（包括整数和小数）。
+ * 例如，字符串"+100"、"5e2"、"-123"、"3.1416"、"-1E-16"、"0123"都表示数值，
+ * 但"12e"、"1a3.14"、"1.2.3"、"+-5"及"12e+5.4"都不是。
+
+ */
+public class offer_20 {
+    public boolean isNumber(String s) {
+        if (s == null || s.length() == 0) return false;
+        //去掉首位空格
+        s = s.trim();
+        boolean numFlag = false;
+        boolean dotFlag = false;
+        boolean eFlag = false;
+        for (int i = 0; i < s.length(); i++) {
+            //判定为数字，则标记numFlag
+            if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+                numFlag = true;
+                //判定为.  需要没出现过.并且没出现过e
+            } else if (s.charAt(i) == '.' && !dotFlag && !eFlag) {
+                dotFlag = true;
+                //判定为e，需要没出现过e，并且出过数字了
+            } else if ((s.charAt(i) == 'e' || s.charAt(i) == 'E') && !eFlag && numFlag) {
+                eFlag = true;
+                numFlag = false;//为了避免123e这种请求，出现e之后就标志为false
+                //判定为+-符号，只能出现在第一位或者紧接e后面
+            } else if ((s.charAt(i) == '+' || s.charAt(i) == '-') && (i == 0 || s.charAt(i - 1) == 'e' || s.charAt(i - 1) == 'E')) {
+
+                //其他情况，都是非法的
+            } else {
+                return false;
+            }
+        }
+        return numFlag;
+    }
+}
