a52e07fe41b7264acb556a26e808de6960a0e94a
wangjj <lzx4627@163.com>
2020-09-21 17:15:03 +0800

    :sparkles: feat:101.对称二叉树

---
+++ src/main/java/com/cicisp/exercise/leetcode_101.java
@@ -0,0 +1,47 @@
+package com.cicisp.exercise;
+
+/**
+ * 给定一个二叉树，检查它是否是镜像对称的。
+
+ 
+
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+ 
+
+但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class leetcode_101 {
+    public boolean isSymmetric(TreeNode root) {
+        return check(root,root);
+    }
+    public boolean check(TreeNode p, TreeNode q){
+        if(p == null && q == null){
+            return true;
+        }
+        if(p == null || q == null){
+            return false;
+        }
+        return p.val == q.val && check(q.right,p.left) && check(q.left,p.right);
+    }
+}
