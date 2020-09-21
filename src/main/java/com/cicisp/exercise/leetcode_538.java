acddd382e496e84f84b72e017ed1f10429450e67
wangjj <lzx4627@163.com>
2020-09-21 10:41:33 +0800

    :sparkles: feat:538.把二叉搜索树转换为累加树

---
+++ src/main/java/com/cicisp/exercise/leetcode_538.java
@@ -0,0 +1,39 @@
+package com.cicisp.exercise;
+/**
+ * 给定一个二叉搜索树（Binary Search Tree），把它转换成为累加树（Greater Tree)，使得每个节点的值是原来的节点值加上所有大于它的节点值之和。
+
+例如：
+
+输入: 原始二叉搜索树:
+              5
+            /   \
+           2     13
+
+输出: 转换为累加树:
+             18
+            /   \
+          20     13
+
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class leetcode_538 {
+    int sum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        if(root != null){
+            convertBST(root.right);
+            sum += root.val;
+            root.val = sum;
+            convertBST(root.left);
+        }
+        return root;
+    }
+}
