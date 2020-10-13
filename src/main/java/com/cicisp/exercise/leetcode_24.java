4f6b1c1aea5eefc8ec5fccdd5a53a8fd1887808d
wangjj <lzx4627@163.com>
2020-10-13 09:33:08 +0800

    :sparkles: feat:24.两两交换链表中的节点

---
+++ src/main/java/com/cicisp/exercise/leetcode_24.java
@@ -0,0 +1,34 @@
+package com.cicisp.exercise;
+
+/**
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+
+你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+
+示例:
+
+给定 1->2->3->4, 你应该返回 2->1->4->3.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+public class leetcode_24 {
+    public ListNode swapPairs(ListNode head) {
+        if(head ==null || head.next == null){
+            return head;
+        }
+        ListNode newHead = head.next;
+        head.next = swapPairs(newHead.next);
+        newHead.next = head;
+        return newHead;
+    }
+}
