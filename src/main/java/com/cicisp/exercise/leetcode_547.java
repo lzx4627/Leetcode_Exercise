e2f895c654ab5095e2345a2b06952b9b8bcad943
wangjj <lzx4627@163.com>
2021-01-13 16:15:14 +0800

    :sparkles: 547.省份数量

---
+++ src/main/java/com/cicisp/exercise/leetcode_547.java
@@ -0,0 +1,36 @@
+package com.cicisp.exercise;
+
+public class leetcode_547 {
+    public int findCircleNum(int[][] isConnected) {
+        int provinces = isConnected.length;
+        int[] parent = new int[provinces];
+        for (int i = 0; i < provinces; i++) {
+            parent[i] = i;
+        }
+        for (int i = 0; i < provinces; i++) {
+            for (int j = i + 1; j < provinces; j++) {
+                if (isConnected[i][j] == 1) {
+                    union(parent, i, j);
+                }
+            }
+        }
+        int circles = 0;
+        for (int i = 0; i < provinces; i++) {
+            if (parent[i] == i) {
+                circles++;
+            }
+        }
+        return circles;
+    }
+
+    public void union(int[] parent, int index1, int index2) {
+        parent[find(parent, index1)] = find(parent, index2);
+    }
+
+    public int find(int[] parent, int index) {
+        if (parent[index] != index) {
+            parent[index] = find(parent, parent[index]);
+        }
+        return parent[index];
+    }
+}
