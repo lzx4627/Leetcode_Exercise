7840afbcef0c13525455ca1bce9814148aa64cf6
wangjj <lzx4627@163.com>
2021-01-13 16:14:38 +0800

    :sparkles: 684.冗余连接

---
+++ src/main/java/com/cicisp/exercise/leetcode_684.java
@@ -0,0 +1,32 @@
+package com.cicisp.exercise;
+
+public class leetcode_684 {
+    public int[] findRedundantConnection(int[][] edges) {
+        int nodesCount = edges.length;
+        int[] parent = new int[nodesCount + 1];
+        for (int i = 1; i <= nodesCount; i++) {
+            parent[i] = i;
+        }
+        for (int i = 0; i < nodesCount; i++) {
+            int[] edge = edges[i];
+            int node1 = edge[0], node2 = edge[1];
+            if (find(parent, node1) != find(parent, node2)) {
+                union(parent, node1, node2);
+            } else {
+                return edge;
+            }
+        }
+        return new int[0];
+    }
+
+    public void union(int[] parent, int index1, int index2) {
+        parent[find(parent, index1)] = find(parent, index2);
+    }
+
+    public int find(int[] parent, int index) {
+        if (parent[index] != index) {
+            parent[index] = find(parent, parent[index]);
+        }
+        return parent[index];
+    }
+}
