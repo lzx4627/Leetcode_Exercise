5467d908c5b171c41b2a6ca29bf82daf9a586a81
wangjj <lzx4627@163.com>
2021-01-20 14:17:38 +0800

    :sparkles: 628.三个数的最大乘积

---
+++ src/main/java/com/cicisp/exercise/leetcode_628.java
@@ -0,0 +1,54 @@
+package com.cicisp.exercise;
+
+/**
+ * 给定一个整型数组，在数组中找出由三个数组成的最大乘积，并输出这个乘积。
+
+示例 1:
+
+输入: [1,2,3]
+输出: 6
+示例 2:
+
+输入: [1,2,3,4]
+输出: 24
+注意:
+
+给定的整型数组长度范围是[3,104]，数组中所有的元素范围是[-1000, 1000]。
+输入的数组中任意三个数的乘积不会超出32位有符号整数的范围。
+ */
+public class leetcode_628 {
+    public int maximumProduct(int[] nums) {
+        // 排序
+        // Arrays.sort(nums);
+        // int n = nums.length;
+        // return Math.max(nums[0] * nums[1] * nums[n-1] , nums[n-3] * nums[n-2] * nums[n-1]);
+
+        // 线性扫描
+        // 最小的和第二小的
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        // 最大的、第二大的和第三大的
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+
+        for (int x : nums) {
+            if (x < min1) {
+                min2 = min1;
+                min1 = x;
+            } else if (x < min2) {
+                min2 = x;
+            }
+
+            if (x > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = x;
+            } else if (x > max2) {
+                max3 = max2;
+                max2 = x;
+            } else if (x > max3) {
+                max3 = x;
+            }
+        }
+
+        return Math.max(min1 * min2 * max1, max1 * max2 * max3);
+    }
+}
