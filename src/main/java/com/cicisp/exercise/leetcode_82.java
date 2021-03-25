fb0305d99ff6e390970340743c6ec4a4f02c43df
wangjj <lzx4627@163.com>
2021-03-25 10:59:48 +0800

    :sparkles: 82.删除排序链表中的重复元素 II

---
+++ src/main/java/com/cicisp/exercise/leetcode_82.java
@@ -0,0 +1,41 @@
+package com.cicisp.exercise;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+public class leetcode_82 {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null){
+            return head;
+        }
+
+        ListNode res = new ListNode(0, head);
+        ListNode cur = res;
+
+        while (cur.next != null && cur.next.next != null){
+            if (cur.next.val == cur.next.next.val){
+                int x = cur.next.val;
+                while (cur.next != null && cur.next.val == x){
+                    cur.next = cur.next.next;
+                }
+            }else{
+                cur = cur.next;
+            }
+        }
+        return res.next;
+    }
+}
