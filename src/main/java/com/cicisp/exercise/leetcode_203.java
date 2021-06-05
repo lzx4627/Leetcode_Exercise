eaa2fba63cc5ca94eb7d4547e494ed6d73a3daf4
wangjj <lzx4627@163.com>
2021-06-05 11:39:30 +0800

    :sparkles: 203.移除链表元素

---
+++ src/main/java/com/cicisp/exercise/leetcode_203.java
@@ -0,0 +1,59 @@
+package com.cicisp.exercise;
+
+/**
+ * 203. 移除链表元素
+ * 给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+ 
+示例 1：
+
+输入：head = [1,2,6,3,4,5,6], val = 6
+输出：[1,2,3,4,5]
+示例 2：
+
+输入：head = [], val = 1
+输出：[]
+示例 3：
+
+输入：head = [7,7,7,7], val = 7
+输出：[]
+ 
+提示：
+
+列表中的节点在范围 [0, 104] 内
+1 <= Node.val <= 50
+0 <= k <= 50
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class leetcode_203 {
+    public ListNode removeElements(ListNode head, int val) {
+        // 递归
+        // if (head == null){
+        //     return head;
+        // }
+        // head.next = removeElements(head.next, val);
+        // return head.val == val ? head.next : head;
+
+        // 迭代
+        ListNode res = new ListNode(0);
+        res.next = head;
+        ListNode tmp = res;
+        while (tmp.next != null){
+            if (tmp.next.val == val){
+                tmp.next = tmp.next.next;
+            }else{
+                tmp = tmp.next;
+            }
+        }
+        return res.next;
+    }
+}
